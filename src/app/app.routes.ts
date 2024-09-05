1a3edfc3c02c354e5e6fe0ecdc7709db3c501f6c
Phiraphat Netnai <zakaki651@gmail.com>
2024-09-05 15:33:46 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,25 +6,28 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
- {
+   { 
+    path: '', redirectTo: 'home', pathMatch: 'full' 
+   },
+   {
     path:'home',
     component: HomeComponent
- },
- {
+   },
+   {
     path:'about',
     component: AboutComponent
- },
- {
+   },
+   {
     path:'contact',
     component: ContactComponent
- },
- {
+   },
+   {
     path:'login',
     component: LoginComponent
- },
- {
+   },
+   {
     path:'register',
     component: RegisterComponent
- },
+   },
 ];
 
